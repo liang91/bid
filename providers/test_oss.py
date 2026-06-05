d51d0117cd5a548eaf833efd1d51ebf9c8794791
liang <guol@eol.cn>
2026-06-05 18:56:42 +0800

    remove ak

--- providers/test_oss.py
+++ providers/test_oss.py
@@ -3,10 +3,10 @@ from .oss import OSS
 
 def test_put():
     local = '/home/liang/work/bid/html/1779775906908632.html'
-    remote_key = 'html/1779775906908632.html'
+    remote_key = 'html/2.html'
     print(OSS.put(local, remote_key))
 
 def test_get():
-    local = '/home/liang/work/bid/html/1.html'
-    remote_key = 'html/1779775906908632.html'
+    local = '/home/liang/work/bid/html/2.html'
+    remote_key = 'html/2.html'
     print(OSS.get(remote_key, local))
